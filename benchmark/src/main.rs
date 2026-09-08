deadc0340474c963d7493e21bc8256095c04286e
at-robins <30956028+at-robins@users.noreply.github.com>
2026-09-08 08:48:03 +0200

    [0] Added parallel input file processing
    Multiple files are now processed in parallel.

--- benchmark/src/main.rs
+++ benchmark/src/main.rs
@@ -1,18 +1,24 @@
-use std::path::PathBuf;
+use std::{
+    path::PathBuf,
+    sync::{Arc, Mutex},
+};
 
 use clap::Parser;
+
+use rayon::prelude::*;
 
 use crate::{arguments::CommandLineArguments, output::OutputData};
 
 fn main() -> Result<(), String> {
     let clas = CommandLineArguments::parse();
 
-    let mut output_data = Vec::new();
-    for peak_file in clas.input_file_peaks() {
-        output_data
-            .push(OutputData::compute::<&PathBuf, &PathBuf>(peak_file, clas.input_file_counts())?);
-    }
-    OutputData::save_results(&output_data, clas.output_file())
+    let output_data = Arc::new(Mutex::new(Vec::new()));
+    clas.input_file_peaks().par_iter().for_each(|peak_file| {
+        let result_output =
+            OutputData::compute::<&PathBuf, &PathBuf>(peak_file, clas.input_file_counts()).unwrap();
+        output_data.lock().unwrap().push(result_output);
+    });
+    OutputData::save_results(&output_data.lock().unwrap(), clas.output_file())
 }
 
 mod arguments;
